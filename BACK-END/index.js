8796b948b9657918910c178011553c7ae8c1e9d1
Rafael Ribeiro <klkp.398@gmail.com>
2025-07-05 11:03:23 -0300

    Criação da pasta front-end com REACT

---
+++ BACK-END/index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const cors = require('cors');
+
+const app = express();
+app.use(express.urlencoded({ extend: true}));
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3333;
+
+app.listen(PORT, () => 
+{console.log("Tudo funcionando chefia 😎👍")}
+);
