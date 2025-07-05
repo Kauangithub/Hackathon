c6e7c948257df825669e602c9ea0ba70d9ab6988
Rafael Ribeiro <klkp.398@gmail.com>
2025-07-05 11:21:01 -0300

    requisição mysql

--- BACK-END/index.js
+++ BACK-END/index.js
@@ -12,3 +12,10 @@ app.listen(PORT, () =>
 {console.log("Tudo funcionando chefia 😎👍")}
 );
 
+const mysql = require('mysql2/promise')
+const connection = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: ''
+})
