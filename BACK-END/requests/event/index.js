271629e25589d027d6fb30e4dcbee108e1acf48f
Ygor <fatalrestart1@gmail.com>
2025-07-06 11:01:43 -0300

    post eventos

--- BACK-END/requests/event/index.js
+++ BACK-END/requests/event/index.js
@@ -1,18 +1,46 @@
+import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient()
 
-app.post('/Eventos', async (req, res) => {
+import { app } from "../../index.js"
+function ReqEvent() {   
+    app.post('/eventos', async (req, res) => {
 
-    await prisma.event.create({
-        data: {
-            title: req.body.title,
-            desc: req.body.desc,
-            dateend: req.body.dateend,
-            datestart: req.body.datestart,
-        }
-    }) 
+        await prisma.event.create({
+            data: {
+                title: req.body.title,
+                desc: req.body.desc,
+                dateend: req.body.dateend,
+                datestart: req.body.datestart,
+                status: ACCEPT
+                // status tá accept para desenvolvimento
+            }
+        }) 
 
-    res.status(201).json(req.body)
+        res.status(201).json(req.body)
 
-});
+    });
 
-export default app;
+    // busca os eventos aceitos
+    app.get('/eventos', async (req, res) => {
+            let events = [];
+    
+            if (req.query) {
+                events = await prisma.event.findMany({
+                    where: {
+                        title: req.query.title,
+                        datestart: req.query.datestart,
+                        dateend: req.query.dateend,
+                        status: ACCEPT
+                    }
+                })
+            } else {
+                users = await prisma.user.findMany()
+            };
+    
+            res.status(200).json(users)
+        })
+
+}
+
+export {ReqEvent};  
