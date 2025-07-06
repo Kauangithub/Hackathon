1d2ae2cdf4ffb979a141f59705cfcfeaee056f5b
Rafael Ribeiro <klkp.398@gmail.com>
2025-07-06 19:45:04 -0300

    Preferências

--- front-end/src/pages/Preferences.js
+++ front-end/src/pages/Preferences.js
@@ -5,6 +5,30 @@ function Preferences() {
                 <h3>Preferências de eventos</h3>
                 <p>Para que possamos recomendar os melhores eventos, selecione suas categorias e horários preferidos. Assim, você recebe sugestões que são a sua cara!</p>
                 <p>Selecione uma ou mais opções</p> 
+                <h2>Tipos de Eventos</h2>
+                <div>
+                    <h2>Tipos de Eventos</h2>
+                    <h5>Culinária</h5>
+                    <h5>Shows</h5>
+                    <h5>Arte</h5>
+                    <h5>Passeios</h5>
+                    <h5>Trilhas</h5>
+                    <h5>Compras</h5>
+                    <h5>Lazer</h5>
+                    <h5>Cultura</h5>
+                    <h5>Esportes</h5>
+                </div>
+                <div>
+                    <h2>Horários</h2>
+                    <h5>Manhã</h5>
+                    <h5>Tarde</h5>
+                    <h5>Noite</h5>
+                    <h5>Madrugada</h5>
+                </div>
+                <button type='submit' className='border horizontal'><p>Concluir</p></button>
+                <div>
+                    
+                </div>
             </div>
         </>
     )
